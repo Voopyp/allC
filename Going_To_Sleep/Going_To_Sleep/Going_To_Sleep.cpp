8f45e4acd6742f765b38f38d0285767005ea48fa
Voopyp <155829802+Voopyp@users.noreply.github.com>
2025-04-03 16:55:50 +0800

    Add files via upload

---
+++ Going_To_Sleep/Going_To_Sleep/Going_To_Sleep.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <csignal>
+
+using namespace std;
+void signalHandler(int signum)
+{
+    cout << "Interput aignal(" << signum << ")received. \n";
+
+    //cleanup and close up stuf here
+    //terminate program
+
+    exit(signum);
+}
+int main()
+{
+    int i = 0;
+    signal(SIGINT, signalHandler);
+
+    while (++i) {
+        cout << "Going to sleep..." << endl;
+        if (i == 3 ) {
+            raise(SIGINT);
+        }
+    }
+    return 0;
+}
+
